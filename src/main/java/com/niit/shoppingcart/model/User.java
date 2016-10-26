272050cdcb47f977466974b07f32c412e7884ce5
Aswathigupta <aswathi.g25@gmail.com>
2016-10-26 11:27:59 +0530

    hibernate validation

--- src/main/java/com/niit/shoppingcart/model/User.java
+++ src/main/java/com/niit/shoppingcart/model/User.java
@@ -8,6 +8,8 @@ import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.stereotype.Component;
 
@@ -16,17 +18,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class User implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+
 	@Id
 	private String id;
 
+	@NotNull(message="Name cannot be null")
+	@Size(min=2, max=30)
 	private String Name;
 
+	 @NotNull(message="Password cannot be null")
+	 @Size(min=4)
 	private String password;
 
+	 @NotNull(message="EmailID cannot be null")
 	private String EmailID;
 
+	@NotNull(message="Address cannot be null")
 	private String Address;
 
+	@NotNull(message="ContactNumber cannot be null")
+	@Size(max=10)
 	private String ContactNumber;
 
 	private boolean isAdmin;
